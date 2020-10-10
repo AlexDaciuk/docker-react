49db1bdffb30240265627f1774d4ac8ec882a4e9
Alexis Daciuk <adaciuk@gmail.com>
2020-10-09 21:42:28 -0300

    Made a change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Already edited <code>src/App.js</code>
+          Already edited 2 times <code>src/App.js</code>
         </p>
         <a
           className="App-link"
